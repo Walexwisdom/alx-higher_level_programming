b4c567ef17f3f918fe6672003381c2644a9ade5f
Adewale Noah Alao <80969134+Walexwisdom@users.noreply.github.com>
2022-07-05 02:53:09 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+for number in range(100):
+    if (number != 99):
+        print("{}{}, ".format(int(number / 10), number % 10), end="")
     else:
-        print("{:02}".format(number), end=", ")
+        print("{}{}".format(int(number / 10), number % 10))
