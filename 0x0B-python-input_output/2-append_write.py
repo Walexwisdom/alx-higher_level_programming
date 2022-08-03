8035e8af3bfdd6a2768c8b682bb7784dcbd2d17b
Adewale Noah Alao <80969134+Walexwisdom@users.noreply.github.com>
2022-08-03 22:35:39 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-'''A module containing IO functions.
-'''
+"""
+appends a string at the end of a text file (UTF8)
+and returns the number of characters added
+"""
 
 
 def append_write(filename="", text=""):
-    '''Appends a UTF-8 encoded text to a file.
-    Args:
-        filename (str): The name of the file to write to.
-        text (str): The content to store in the file.
-    Returns:
-        int: The number of characters written.
-    '''
-    n = 0
-    with open(filename, mode='a', encoding='utf-8') as file:
-        n = file.write(text)
-    return 
+    """
+    appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
